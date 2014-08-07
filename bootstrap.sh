6c0468867641fcbb3b6096c6121548a28a201129
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2014-08-07 19:54:16 -0400

    Install Postgres

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,8 @@
 set -eo pipefail
 
 wget -qO- https://raw.github.com/progrium/dokku/v0.2.3/bootstrap.sh | sudo DOKKU_TAG=v0.2.3 bash
+
+cd /var/lib/dokku/plugins
+git clone https://github.com/jeffutter/dokku-postgresql-plugin postgresql
+dokku plugins-install
+dokku postgresql:start
